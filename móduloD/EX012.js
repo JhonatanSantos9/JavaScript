1de4ba47a385da7e14d095e07cef809571868ae4
JhonatanSantos9 <jhonatansantos759.js@gmail.com>
2023-02-03 00:27:45 -0300

    \5fdhctgcfn

---
+++ móduloD/EX012.js
@@ -0,0 +1,11 @@
+var hora = 03
+console.log(`Agora são ${hora} horas.`)
+if (hora > 6){
+    console.log('Bom dia!')
+} else if(hora <= 18){
+    console.log('Boa Tarde!')
+} else if(hora <= 00){
+    console.log('Boa noite PUTA!')
+} else{
+    console.log('Boa madrugada piranha! <3')    
+}
