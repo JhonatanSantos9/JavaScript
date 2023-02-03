27f7827e3994d75ddce6f9c91c138b1b47d02363
JhonatanSantos9 <jhonatansantos759.js@gmail.com>
2023-02-02 22:59:27 -0300

    sla awujhfdalihejfn

---
+++ móduloC/EX011.js
@@ -0,0 +1,10 @@
+var idade = 0.5
+console.log(`você tem ${idade} anos`)
+if (idade < 16){
+    console.log('vc não vota viado')
+}else if (idade < 18 || idade >65) {
+        console.log('Se quiser votar vota bixa!')
+}else {
+        console.log('voto obrigatório mô')
+}
+
---
+++ móduloD/EX004.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+
+console.log(`a velocidade da tu lata velha é ${vel} km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO GAY!')
+}
+console.log('Dirija sempre usando sinto de segurança')
