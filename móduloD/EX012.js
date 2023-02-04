3e426b441fc0512abb69accca0da915f2edf1466
JhonatanSantos9 <jhonatansantos759.js@gmail.com>
2023-02-03 23:27:28 -0300

    sim

--- móduloD/EX012.js
+++ móduloD/EX012.js
@@ -1,6 +1,7 @@
-var hora = 03
+var agora = new Date()
+var hora = agora.getHours
 console.log(`Agora são ${hora} horas.`)
-if (hora > 6){
+if (hora < 12){
     console.log('Bom dia!')
 } else if(hora <= 18){
     console.log('Boa Tarde!')
---
+++ móduloD/EX014/script.js
@@ -0,0 +1,23 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} hora(s).`
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        img.src = 'manha.png'
+        document.body.style.background = 'bisque'
+    } else if(hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = 'orange'
+    } else{
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = 'blue'
+    }
+}
+
+
