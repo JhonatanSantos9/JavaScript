54c09047cea954ec15ce402fe1ac2b2552c874a6
Jhonatan Santos <102446752+JhonatanSantos9@users.noreply.github.com>
2023-04-05 10:27:21 -0300

    Update script.js

--- móduloD/EX014/script.js
+++ móduloD/EX014/script.js
@@ -2,13 +2,13 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22
-    msg.innerHTML = `Agora são ${hora} hora(s).`
+    var hora = data.getHours()
+    var minuto = data.getMinutes() 
+    msg.innerHTML = `Agora são ${hora} hora(s) e ${minuto} minuto(s).`
     if (hora >= 0 && hora < 12){
         //bom dia
         img.src = 'manha.png'
-        document.body.style.background = 'bisque'
+        document.body.style.backgroundColor = 'rgb(242, 196, 140)'
     } else if(hora >= 12 && hora < 18){
         //boa tarde
         img.src = 'tarde.png'
@@ -19,5 +19,7 @@ function carregar(){
         document.body.style.background = 'blue'
     }
 }
+
+    
 
 
