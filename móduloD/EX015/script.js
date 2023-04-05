66b10c9911e16a74c6173b8fc15e4cede191c3f5
Jhonatan Santos <102446752+JhonatanSantos9@users.noreply.github.com>
2023-04-05 11:22:03 -0300

    defaqdw

--- móduloD/EX015/script.js
+++ móduloD/EX015/script.js
@@ -9,11 +9,43 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
         if (fsex[0].checked) {
             gênero = 'homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'criançah.jpg')
+            } else if (idade < 21) { 
+                //jovem
+                img.setAttribute('src', 'jovemh.jpg')
+            } else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'adultoh.jpg')
+            } else{
+                //idoso
+                img.setAttribute('src', 'idoso.jpg')
+            }
         }else if (fsex[1].checked){
             gênero = 'mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'criançam.jpg')
+            } else if (idade < 21) { 
+                //jovem
+                img.setAttribute('src', 'jovemm.jpg')
+            } else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'adutom.jpg')
+            } else{
+                //idoso
+                img.setAttribute('src', 'idosa.jpg')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+        img.setAttribute('width', '200px')
+        img.setAttribute('height', '200px')
+        img.setAttribute('display', 'block')
     }
 }
