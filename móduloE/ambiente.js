b07ea5776a1aeafd68a9b95c7f0af43e86c1fae2
Jhonatan Santos <102446752+JhonatanSantos9@users.noreply.github.com>
2023-04-19 10:44:47 -0300

    Update ambiente.js

--- móduloE/ambiente.js
+++ móduloE/ambiente.js
@@ -1,5 +1,23 @@
-var c = 1
+var num = 1
+
 do{
-    console.log('cu')
+    console.log('oie')
+    num++
+}while(num <= 6)
+
+
+/*
+var c = 1
+while (c <= 6){
+    console.log('tudo bem')
     c++
-} while (c < 6)
+}*/
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
