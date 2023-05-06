dccb88dd15757136c88804134e85d4ecc5f85bd4
Jhonatan Santos <102446752+JhonatanSantos9@users.noreply.github.com>
2023-05-06 06:47:29 -0300

    a

--- móduloF/ambiente.js
+++ móduloF/ambiente.js
@@ -4,7 +4,3 @@ num.sort()
 console.log(num)
 console.log(`O meu vetor tem ${num.length} posições`)
 console.log(`O primeiro valor é o valor ${num[0]}`)
-
-for(let pos = 0;pos < num.length;pos++){
-    console.log(`A posição ${pos} tem o valor de ${num[pos]}`)
-}
---
+++ móduloF/valorestela.js
@@ -0,0 +1,13 @@
+let num = [8, 1, 7, 4, 2, 9]
+
+for(let pos in num){
+    console.log(num[pos])
+}
+
+
+
+/*
+valores.sort()
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição:${pos}, tem o valor:${valores[pos]}`)
+}*/
